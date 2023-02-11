d1df6684083e172b740458fe8ea11acbac4fd6e3
chukssomzzy <chukssomzzy19@gmail.com>
2023-02-11 14:41:10 +0100

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -3,9 +3,12 @@
 """BaseModel defines all the common attributes/methods"""
 import uuid
 import datetime
+from models import storage
+
 
 class BaseModel:
     def __init__(self, *args, **kargs):
+        """initialize the basemodel from a dict"""
         if kargs:
             for key, val in kargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -16,12 +19,18 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
+            storage.new(self.to_dict())
     def __str__(self):
+        """Returns a string representation of the basemodel"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
     def save(self):
+        """Save changes to storage"""
         self.updated_at = datetime.datetime.now()
+        storage.new(self.to_dict())
+        storage.save()
     def to_dict(self):
-        dict = self.__dict__
+        """Returns a dictionary representation of the obj"""
+        dict = self.__dict__.copy()
         dict["__class__"] = self.__class__.__name__
         dict["created_at"] = self.created_at.isoformat()
         dict["updated_at"] = self.updated_at.isoformat()
---
+++ models/engine/file_storage.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""serialize and deserialize python obj"""
+
+import json
+
+
+class FileStorage:
+    """File strorage engine"""
+    __file_path = "file.json"
+    __objects = {}
+    iter = 0
+    def all(self):
+        """Returns the dictionary object"""
+        return self.__objects
+    def new(self, obj):
+        """set __objectS with dictionary value of object
+
+        Args:
+            obj: obj to serialize
+        """
+        if obj and dict == type(obj):
+            self.__objects[obj["__class__"] + "." + obj["id"]] = obj
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        with open(self.__file_path, "w", encoding="utf8") as f:
+            json.dump(self.__objects, f)
+    def reload(self):
+        """deserializes the JSON file to __objects (only if the JSON
+            __file_path exits; otherwise, do nothing, if the file doesn't
+            exist, no exception should be raised)
+        """
+        try:
+            with open(self.__file_path, "r", encoding="utf8") as f:
+                self.__objects = json.load(f)
+        except FileNotFoundError:
+            pass
+
